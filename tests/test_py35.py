e9e35e15b6ba839a31b79c1351df06f0b6fa086f
Andrew Svetlov <andrew.svetlov@gmail.com>
2017-08-23 10:49:57 +0300

    Fix tests

--- tests/test_py35.py
+++ tests/test_py35.py
@@ -7,11 +7,12 @@ from async_timeout import timeout
 
 async def test_async_timeout(loop):
     with pytest.raises(asyncio.TimeoutError):
-        async with timeout(0.01, loop=loop):
-            yield from asyncio.sleep(10, loop=loop)
+        async with timeout(0.01, loop=loop) as cm:
+            await asyncio.sleep(10, loop=loop)
+    assert cm.expired
 
 
 async def test_async_no_timeout(loop):
-    with pytest.raises(asyncio.TimeoutError):
-        async with timeout(1, loop=loop):
-            yield from asyncio.sleep(0, loop=loop)
+    async with timeout(1, loop=loop) as cm:
+        await asyncio.sleep(0, loop=loop)
+    assert not cm.expired
