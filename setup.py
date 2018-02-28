9d2411cc75e783b0d7d5d8a5534cb9e5db8ab38c
Hunter Thompson <thompson.grey.hunter@gmail.com>
2018-02-28 07:05:32 -0700

    Removed import causing ImportError. Refactored to accomadate change

--- setup.py
+++ setup.py
@@ -1,26 +1,13 @@
-import pathlib
-import re
-
 from setuptools import setup
-
-here = pathlib.Path(__file__).parent
-fname = here / 'async_timeout' / '__init__.py'
-
-with fname.open() as fp:
-    try:
-        version = re.findall(r"^__version__ = '([^']+)'$", fp.read(), re.M)[0]
-    except IndexError:
-        raise RuntimeError('Unable to determine version.')
 
 
 def read(name):
-    fname = here / name
-    with fname.open() as f:
+    with open(name) as f:
         return f.read()
 
 
 setup(name='async-timeout',
-      version=version,
+      version='2.0.0',
       description=("Timeout context manager for asyncio programs"),
       long_description='\n\n'.join([read('README.rst'),
                                     read('CHANGES.rst')]),
