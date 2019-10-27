87cdd1f5cc53952f4e5a21ad6f33e6ac0b789e3b
Andrew Svetlov <andrew.svetlov@gmail.com>
2019-10-27 11:03:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import pathlib
 import re
-import sys
 
 from setuptools import setup
 
@@ -20,25 +19,29 @@ def read(name):
         return f.read()
 
 
-setup(name='async-timeout',
-      version=version,
-      description=("Timeout context manager for asyncio programs"),
-      long_description='\n\n'.join([read('README.rst'),
-                                    read('CHANGES.rst')]),
-      classifiers=[
-          'License :: OSI Approved :: Apache Software License',
-          'Intended Audience :: Developers',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6',
-          'Topic :: Internet :: WWW/HTTP',
-          'Framework :: AsyncIO',
-      ],
-      author='Andrew Svetlov',
-      author_email='andrew.svetlov@gmail.com',
-      url='https://github.com/aio-libs/async_timeout/',
-      license='Apache 2',
-      packages=['async_timeout'],
-      python_requires='>=3.5.3',
-      include_package_data=True)
+setup(
+    name='async-timeout',
+    version=version,
+    description=("Timeout context manager for asyncio programs"),
+    long_description='\n\n'.join([read('README.rst'),
+                                  read('CHANGES.rst')]),
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Internet :: WWW/HTTP',
+        'Framework :: AsyncIO',
+    ],
+    author='Andrew Svetlov',
+    author_email='andrew.svetlov@gmail.com',
+    url='https://github.com/aio-libs/async_timeout/',
+    license='Apache 2',
+    packages=['async_timeout'],
+    python_requires='>=3.5.3',
+    include_package_data=True
+)
